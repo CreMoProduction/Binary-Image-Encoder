675dc4f522e1bc8f07b888e7cd87720947ed0021
CreMoProduction <aleks.delta2013@gmail.com>
2021-01-11 22:22:20 +0200

    minor fix

--- Binary Image Encoder.jsx
+++ Binary Image Encoder.jsx
@@ -1,6 +1,6 @@
 ﻿  
 var path = "D:/Работы АЕ/Vh. 22 Smart Keyframe Reverse/Graphics/Compact UI v2.0/Badges/";
-var subolder="Binary Badges"
+var subfolder="Binary Badges"
 var name= "03 Reverse Explorer"
 
 //-----------------------------------------------
@@ -10,11 +10,11 @@ f.open('e');
 
 var strlength;
 var binary;
-strlength= binary.length; //определить длину строки
-binary=binary.substring(12, strlength); //удалить первые 12 символов из строки
-binary=binary.slice(0, -2); //удалить 2 последних символа
+strlength= binary.length; //определить длину строки / define string length
+binary=binary.substring(12, strlength); //удалить первые 12 символов из строки / remove first 12 symbols
+binary=binary.slice(0, -2); //удалить 2 последних символа / remove last 2 symbols
 
-var myFile = new File(path+subolder+"/"+name+".txt");
+var myFile = new File(path+subfolder+"/"+name+".txt");
         myFile.open("w");
         myFile.encoding = "UTF-8";
         myFile.write(binary);
